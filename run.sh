1b63725130bc36a1f3fd7d27bca9821b168689d8
Andrew Prokhorenkov <aprokh@uchicago.edu>
2026-04-01 09:11:21 -0500

    Rename ID to PREFIX in run.sh
    Change variable name from ID to PREFIX for log file naming.

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ source .venv/bin/activate
 
 pip install --no-build-isolation --editable .
 
-ID="ia-$(date +%Y%m%d)-$(date +%H%M%S)-$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 4)"
+PREFIX="ia-$(date +%Y%m%d)-$(date +%H%M%S)-$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 4)"
+echo "$PREFIX"
 
-time python -u src/esbuild/bin/single.py "$ID" > ~/logs/mmrf-esbuild/"$ID".log 2>&1
+time python -u src/esbuild/bin/single.py "$PREFIX" > ~/logs/mmrf-esbuild/"$ID".log 2>&1
